7dd2795acb0c5d9ba4dd2fc60317b791bd951436
Sukhwinder <singhdevelops@gmail.com>
2024-09-26 10:38:33 +0100

    learning express js

--- getting-started/app.js
+++ getting-started/app.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
+const rootDir = require('./util/path');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorRoutes = require('./routes/error');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+app.use(errorRoutes);
+
+const server = http.createServer(app);
 
 server.listen(3000);
