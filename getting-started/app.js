690f248327bd3b04ac813b71cea1656b787aa902
Sukhwinder <singhdevelops@gmail.com>
2024-09-30 10:45:34 +0100

    testing out layouts using pug

--- getting-started/app.js
+++ getting-started/app.js
@@ -4,18 +4,23 @@ const path = require('path');
 const rootDir = require('./util/path');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorRoutes = require('./routes/error');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
-app.use(errorRoutes);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: '404 - Page Not Found'});
+});
 
 const server = http.createServer(app);
 
