08a48a78dd4ded55e25a2df33c8433484fc9ab6a
Sukhwinder Singh <s.sunny_1995@hotmail.co.uk>
2024-10-02 10:14:16 +0100

    Delete getting-started directory

--- getting-started/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const productsController = require('../controllers/product');
-
-const router = express.Router();
-
-router.get('/add-product', productsController.getAddProduct);
-router.post('/add-product', productsController.postAddProduct);
-
-module.exports = router;
