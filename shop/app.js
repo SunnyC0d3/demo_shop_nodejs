bbfd439c4e32c82cb2f26b3d2bf36e524e820a53
Sukhwinder <singhdevelops@gmail.com>
2024-10-02 17:37:06 +0100

    added more structure to the app

--- shop/app.js
+++ shop/app.js
@@ -1,7 +1,5 @@
-const http = require('http');
 const express = require('express');
 const path = require('path');
-const rootDir = require('./util/path');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
@@ -15,13 +13,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
